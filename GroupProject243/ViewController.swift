86ca37fe7aac2af1897ffa85b779f6ad59f2defd
Perry Waxman <perry.waxman@gmail.com>
2016-04-14 14:41:45 -0700

    Button animations
    buttons now show above keyboard when entering number

--- GroupProject243/ViewController.swift
+++ GroupProject243/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     //MARK: - Properties
     @IBOutlet var numberTextField: UITextField!
@@ -21,11 +21,14 @@ class ViewController: UIViewController {
     @IBOutlet var baseButton: UIButton!
     @IBOutlet var convertButton: UIButton!
     
+    @IBOutlet var approachConstraint: NSLayoutConstraint!
+    @IBOutlet var convertConstraint: NSLayoutConstraint!
     //MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view
         self.setupUI()
+        
     }
     
     
@@ -43,9 +46,51 @@ class ViewController: UIViewController {
         
     }
     
+    @IBAction func backgroundTap(sender: AnyObject) {
+        //dismiss keyboard
+        self.view.endEditing(true)
+    }
+    
+    
+    
+    //MARK: - UITextFieldDelegate
+    func textFieldDidBeginEditing(textField: UITextField) {
+        self.moveButtonsUp()
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        self.moveButtonsDown()
+    }
     
     //MARK: - UI
+    
+    func moveButtonsUp(){
+        approachConstraint.constant = 350
+        convertConstraint.constant = 350 + 41 + 37
+        
+        UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 1, options: .CurveEaseInOut, animations: {
+            self.view.layoutIfNeeded()
+
+            }) { (done) in
+                //
+        }
+    }
+    
+    func moveButtonsDown(){
+        approachConstraint.constant = 55
+        convertConstraint.constant = 133
+        
+        UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 1, options: .CurveEaseInOut, animations: {
+            self.view.layoutIfNeeded()
+            
+        }) { (done) in
+            //
+        }
+    }
+    
     func setupUI(){
+        self.numberTextField.delegate = self
+        
         //button corners
         let cornerRadius:CGFloat = 4
         convertButton.layer.cornerRadius = cornerRadius
@@ -53,9 +98,9 @@ class ViewController: UIViewController {
         baseButton.layer.cornerRadius = cornerRadius
         
         //time labels
-        systemTimeLabel.text = ""
-        algorithmTimeLable.text = ""
-        resultLabel.text = ""
+        systemTimeLabel.hidden = true
+        algorithmTimeLable.hidden = true
+        resultLabel.hidden = true
     }
 
 
